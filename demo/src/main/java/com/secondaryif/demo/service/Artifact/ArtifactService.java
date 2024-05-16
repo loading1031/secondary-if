333409cce981f521f4d808071ac284af379536d8
loading1031 <tjdans1031@gmail.com>
2024-05-17 01:07:09 +0900

    :sparkles: Feat: create getArtifact Method

--- demo/src/main/java/com/secondaryif/demo/service/Artifact/ArtifactService.java
+++ demo/src/main/java/com/secondaryif/demo/service/Artifact/ArtifactService.java
@@ -1,5 +1,6 @@
 package com.secondaryif.demo.service.Artifact;
 
+import com.secondaryif.demo.domain.Artifact;
 import com.secondaryif.demo.web.dto.artifact.ArtifactReqDto;
 import com.secondaryif.demo.web.dto.artifact.ArtifactResDto;
 
@@ -7,5 +8,6 @@ import java.util.List;
 
 public interface ArtifactService {
     ArtifactResDto.PostResDto postArtifact(ArtifactReqDto.PostDto request, Long memberId);
+    Artifact getArtifact(Long artifactId);
     List<ArtifactResDto.PostResDto> getArtifactList();
 }
