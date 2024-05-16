958fff2f14582b92716213e919dcf8e382a8785a
loading1031 <tjdans1031@gmail.com>
2024-05-17 01:00:56 +0900

    :recycle: Refactor: postArtifact logic and create getArtifact method

--- demo/src/main/java/com/secondaryif/demo/service/Artifact/ArtifactServiceImpl.java
+++ demo/src/main/java/com/secondaryif/demo/service/Artifact/ArtifactServiceImpl.java
@@ -1,9 +1,13 @@
 package com.secondaryif.demo.service.Artifact;
 
+import com.secondaryif.demo.apiPayload.code.status.ErrorStatus;
+import com.secondaryif.demo.apiPayload.exception.GeneralException;
 import com.secondaryif.demo.converter.ArtifactConverter;
 import com.secondaryif.demo.domain.Artifact;
 import com.secondaryif.demo.domain.Member;
+import com.secondaryif.demo.domain.Upload;
 import com.secondaryif.demo.repository.ArtifactRepository;
+import com.secondaryif.demo.repository.UploadRepository;
 import com.secondaryif.demo.service.Member.MemberService;
 import com.secondaryif.demo.web.dto.artifact.ArtifactReqDto;
 import com.secondaryif.demo.web.dto.artifact.ArtifactResDto;
@@ -19,6 +23,7 @@ import java.util.List;
 public class ArtifactServiceImpl implements ArtifactService{
     private final MemberService memberService;
     private final ArtifactRepository artifactRepository;
+    private final UploadRepository uploadRepository;
     @Override
     @Transactional
     public ArtifactResDto.PostResDto postArtifact(ArtifactReqDto.PostDto request, Long memberId) {
@@ -26,7 +31,14 @@ public class ArtifactServiceImpl implements ArtifactService{
         Artifact newArtifact = ArtifactConverter.toPost(request,getMember);
         newArtifact.setArtifact(getMember);
 
+        uploadRepository.save(Upload.builder().content("루트 페이지입니다.").build());
         return ArtifactConverter.toPostResDto(artifactRepository.save(newArtifact));
+    }
+
+    @Override
+    public Artifact getArtifact(Long artifactId) {
+        return artifactRepository.findById(artifactId).orElseThrow(
+                () -> new GeneralException(ErrorStatus._BAD_REQUEST));
     }
 
     @Override
