868694db7bc1e42fb3bb62c6a25c62013a694df6
loading1031 <tjdans1031@gmail.com>
2024-11-07 02:08:02 +0900

    ♻️Refactor: apply Swagger UI

--- server/src/main/java/com/secondaryif/server/web/controller/ArtifactRestController.java
+++ server/src/main/java/com/secondaryif/server/web/controller/ArtifactRestController.java
@@ -7,6 +7,8 @@ import com.secondaryif.server.service.Upload.UploadQueryService;
 import com.secondaryif.server.service.Upload.UploadService;
 import com.secondaryif.server.web.dto.artifact.ArtifactResDto;
 import com.secondaryif.server.web.dto.upload.UploadReqDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,16 +17,19 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/artifacts")
+@Tag(name="Artifact",description = "Artifact Rest API")
 public class ArtifactRestController {
     private final ArtifactService artifactService;
     private final ArtifactQueryService artifactQueryService;
     private final UploadService uploadService;
     private final UploadQueryService uploadQueryService;
     @GetMapping("")
+    @Operation(description = "작품 리스트 조회")
     ApiResult<List<ArtifactResDto.PostResDto>>getArtList(){
         return ApiResult.onSuccess(artifactQueryService.getArtifactList());
     }
     @PostMapping("/{artifactId}/upload")
+    @Operation(description = "업로드 등록")
     ApiResult<?>postUpload(
             @RequestParam(name = "memberId")Long memberId,
             @PathVariable(name = "artifactId") Long artifactId,
@@ -32,29 +37,22 @@ public class ArtifactRestController {
        return ApiResult.onSuccess(uploadService.postUpload(memberId, artifactId, request));
     }
     @GetMapping("/{artifactId}/original")
+    @Operation(description = "원작 조회")
     ApiResult<?>getArtWithOriginUploadList(@PathVariable(name = "artifactId") Long artifactId){
         return ApiResult.onSuccess(artifactService.getArtifactWithOriginUploads(artifactId));
     }
     @GetMapping("/{artifactId}")
+    @Operation(description = "원작/팬아트 전체 조회")
     ApiResult<?>getArtWithTotalUploadList(@PathVariable(name = "artifactId") Long artifactId){
         return ApiResult.onSuccess(artifactService.getArtifactWithTotalUploads(artifactId));
     }
     @GetMapping("/{artifactId}/graph")
+    @Operation(description = "그래프 DB에서 작품 조회")
     ApiResult<?>getArtWithTotalUploadGraphList(@PathVariable(name = "artifactId") Long artifactId){
         return ApiResult.onSuccess(artifactService.getArtifactWithTotalUploadGraphs(artifactId));
     }
     @GetMapping("/{artifactId}/uploads/{endUploadId}/weight")
+    @Operation(description = "최대 가중치 그래프 탐색 방식의 업로드 조회")
     ApiResult<?> getMaxWeightGraph(
             @PathVariable(name="artifactId") Long artifactId,
-            @PathVariable(name="endUploadId") Long endUploadId){
-        return ApiResult.onSuccess(uploadQueryService.getMaxWeightPathDto(artifactId,endUploadId));
-    }
-    @GetMapping("/search")
-    ApiResult<?> searchArtifacts(
-            @RequestParam(name="title",required = false) String title,
-            @RequestParam(name="author",required = false) String author,
-            @RequestParam(name="page", defaultValue= "0") Integer page){
-        return ApiResult.onSuccess(artifactQueryService.searchArtifactList(title,author,page));
-    }
-
-}
+            @PathVariabl
--- server/src/main/java/com/secondaryif/server/web/controller/UploadRestController.java
+++ server/src/main/java/com/secondaryif/server/web/controller/UploadRestController.java
@@ -3,40 +3,37 @@ package com.secondaryif.server.web.controller;
 import com.secondaryif.server.global.apiPayload.ApiResult;
 import com.secondaryif.server.service.Upload.UploadQueryService;
 import com.secondaryif.server.service.Upload.UploadService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/uploads")
+@Tag(name="Upload", description = "Upload Rest API")
 public class UploadRestController {
     private final UploadService uploadService;
     private final UploadQueryService uploadQueryService;
 
     @PostMapping("/{uploadId}/like")
+    @Operation(description = "로그인 및 회원가입")
     ApiResult<?> likeUpload(
             @PathVariable(name = "uploadId") Long uploadId,
             @RequestParam(name = "memberId") Long memberId){
         return ApiResult.onSuccess(uploadService.postLike(uploadId,memberId));
     }
     @GetMapping("/{uploadId}/graph")
+    @Operation(description = "그래프DB 업로드 조회")
     ApiResult<?>getUploadGraph(@PathVariable(name = "uploadId") Long uploadGraphId){
         return ApiResult.onSuccess(uploadQueryService.getUploadGraph(uploadGraphId));
     }
     @GetMapping("/{uploadId}")
+    @Operation(description = "SQLDB 업로드 조회")
     ApiResult<?> getUploadDto(
             @PathVariable(name="uploadId") Long uploadId,
             @RequestParam(name="prevUploadId",required=false, defaultValue="0") Long prevUploadId){
 
         if(prevUploadId != 0)
             return ApiResult.onSuccess(uploadQueryService.getUploadDtoByFetchWeight(prevUploadId,uploadId));
-        else return ApiResult.onSuccess(uploadQueryService.getUploadDto(uploadId));
-    }
-    @PatchMapping("/{uploadId}")
-    ApiResult<?> patchUploadWithChild(
-            @PathVariable(name="uploadId") Long uploadId,
-            @RequestParam(name="nextUploadId") Long nextUploadId
-    ){
-        return ApiResult.onSuccess(uploadService.patchUploadChild(uploadId,nextUploadId));
-    }
-}
+        else return ApiResult.onSuccess(uploadQueryService.ge
